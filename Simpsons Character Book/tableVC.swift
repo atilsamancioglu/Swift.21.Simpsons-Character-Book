72be440734d5163cbb37fa22f9b3402c6024df9d
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-02-12 20:40:29 +0300

    final version

--- Simpsons Character Book/tableVC.swift
+++ Simpsons Character Book/tableVC.swift
@@ -8,18 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class tableVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var mySimpsons = [Simpson]()
+    var chosenSimpson = Simpson()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //tableView setup
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+      
+        
+        //create our characters
+        
+        let homer = Simpson()
+        homer.name = "Homer Simpson"
+        homer.occupation = "Safety Inspector"
+        homer.image = UIImage(named: "homer.png")!
+        
+        let bart = Simpson()
+        bart.name = "Bart Simpson"
+        bart.occupation = "Student"
+        bart.image = UIImage(named: "bart.PNG")!
+        
+        let marge = Simpson()
+        marge.name = "Marge Simpson"
+        marge.occupation = "Homermaker"
+        marge.image = UIImage(named: "marge.png")!
+        
+        let ned = Simpson()
+        ned.name = "Ned Flanders"
+        ned.occupation = "Pharmacist"
+        ned.image = UIImage(named: "ned.png")!
+        
+        mySimpsons.append(homer)
+        mySimpsons.append(bart)
+        mySimpsons.append(marge)
+        mySimpsons.append(ned)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mySimpsons.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = mySimpsons[indexPath.row].name
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! detailsVC
+            destinationVC.selectedSimpson = self.chosenSimpson
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.chosenSimpson = mySimpsons[indexPath.row]
+        self.performSegue(withIdentifier: "toDetailsVC", sender: nil)
+        
+    }
+    
+    
 
 }
 
